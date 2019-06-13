13227a945f88ea591fe0313dce38f48ef6c118c0
Sezal Agrawal <sezalagrawal@Sezals-MacBook-Pro.local>
2019-06-13 10:46:33 +0530

    Level 1 hands on

--- example1.go
+++ example1.go
@@ -7,9 +7,12 @@ var y = 2
 
 //assign 0 for int, 0.0 for float, "" for string, nil for pointers, false for bool
 var z int
+type t int
+var s t
 
 func main() {
 	x := 3
+	s = 5
 	m := `She said,
 	 "Hello"`
 	fmt.Println("Hello", y, x)
@@ -17,9 +20,18 @@ func main() {
 	fmt.Println("Hello", y, x)
 	fmt.Printf("%T\n", z)
 	fmt.Println(m)
+	fmt.Printf("%d\n%x\n%#x", y, y, y)
+	p := fmt.Sprintf("%d\n%x\n%#x", y, y, y)
+	fmt.Println(p)
+	fmt.Printf("%T\n", s)
 
-	// can't change type
+	// can't change type, go is static, not dynamic
 	//x = "sdsd"
+
+	//conversion: convert value of one type to another
+	x = int(s)
+	//can't do x = s, even though underlying type is same
+	//no casting in go
 }
 
 func foo() {
---
+++ level1/handson1.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+	x := 42
+	y := "James Bond"
+	z := true
+
+	fmt.Println(x, y, z)
+	fmt.Printf("%v\n%v\n%v\n", x, y, z)
+}
