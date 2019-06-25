96fcd2b1b1ec82b67c8e9174806a2e4fa47ecf11
Sezal Agrawal <sezalagrawal@Sezals-MacBook-Pro.local>
2019-06-26 01:08:34 +0530

    Level 2 handson

---
+++ level2/handson1.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+)
+
+//create typed and untyped constants
+const(
+	y int = 3
+	z = 4
+)
+
+const(
+	a = 2017 + iota
+	b
+	c
+	d
+)
+
+func main(){
+	x := 3
+	//Print decimal, binary, hex
+	fmt.Printf("%d, %b, %#x\n", x, x, x)
+	fmt.Println(a, b, c, d)
+
+}
+
+//Notes
+//Use int, float64 generic
