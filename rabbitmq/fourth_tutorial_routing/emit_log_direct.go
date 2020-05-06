4e1b628ebf93f83a6254fa623aaa7c541eb64102
sezalagrawal <sezalagrawal@gmail.com>
2020-05-07 00:38:21 +0530

    rabbitmq routing

---
+++ rabbitmq/fourth_tutorial_routing/emit_log_direct.go
@@ -0,0 +1,9 @@
+package main
+
+// Usecase is to subscribe to only a subset of messages
+// Save only critical error messages to log file (to save disk space)
+// whereas print all log messages 
+
+func main() {
+
+}
