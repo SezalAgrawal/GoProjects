5804e536e605f067ce135fa01662aafd02bdd004
sezalagrawal <sezalagrawal@gmail.com>
2020-08-12 22:37:55 +0530

    wip: web crawler

---
+++ concurrency/web_crawler.go
@@ -0,0 +1,86 @@
+package main
+
+import "fmt"
+
+// Usecase: parallelize web crawler to read URLs concurrently
+// and without reading same URL twice
+
+type fetcher interface {
+	fetch(url string) (body string, urls []string, err error)
+}
+
+type fakeResult struct {
+	body string
+	urls []string
+}
+
+type fakeFetcher map[string]*fakeResult
+
+// fetch returns body of the URL
+// and the slice of URLs found on that page
+func (f fakeFetcher) fetch(url string) (string, []string, error) {
+	if res, ok := f[url]; ok {
+		return res.body, res.urls, nil
+	}
+	return "", nil, fmt.Errorf("not found: %s", url)
+}
+
+// fetcher is a populated fakeFetcher.
+var fetcherList = fakeFetcher{
+	"http://golang.org": &fakeResult{
+		"The Go Programming Language",
+		[]string{
+			"http://golang.org/pkg/",
+			"http://golang.org/cmd/",
+		},
+	},
+	"http://golang.org/pkg/": &fakeResult{
+		"Packages",
+		[]string{
+			"http://golang.org",
+			"http://golang.org/cmd/",
+			"http://golang.org/pkg/fmt/",
+			"http://golang.org/pkg/os/",
+		},
+	},
+	"http://golang.org/pkg/fmt/": &fakeResult{
+		"Package fmt",
+		[]string{
+			"http://golang.org",
+			"http://golang.org/pkg/",
+		},
+	},
+	"http://golang.org/pkg/os/": &fakeResult{
+		"Package os",
+		[]string{
+			"http://golang.org",
+			"http://golang.org/pkg/",
+		},
+	},
+}
+
+// crawl uses fetcher to recursively crawl
+// pages starting with url, to a maximum of depth
+func crawl(url string, depth int, f fetcher) {
+	// TODO: Fetch URLs in parallel.
+	// TODO: Don't fetch the same URL twice.
+	// This implementation doesn't do either:
+	if depth <= 0 {
+		return
+	}
+	body, urls, err := fetcher.Fetch(url)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Printf("found: %s %q\n", url, body)
+	for _, u := range urls {
+		Crawl(u, depth-1, fetcher)
+	}
+	return
+
+}
+
+func main() {
+	crawl("http://golang.org", 4, fetcherList)
+}
